680cfcdb9af4179d08ae47818631bbeb4f8bfc31
Anton Volyk <Anton_Volyk@epam.com>
2020-10-24 17:51:31 +0300

    add xml, csv loaders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import Post from './Post';
 import json from './assets/data.json';
+import xml from './assets/data.xml';
+import csv from './assets/data.csv';
 import webpackLogo from './assets/webpack_logo.png';
 import './styles/styles.css';
 
 const post = new Post('Webpack Post Title', webpackLogo);
 
 console.log(`Post to string: ${post}`);
-
 console.log('JSON:', json);
+console.log('XML:', xml);
+console.log('CSV:', csv);
