9e1b2143cbb20737a8bae94a30d5e40dec07285b
Anton Volyk <Anton_Volyk@epam.com>
2020-10-24 17:03:38 +0300

    add file-loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Post from './Post';
+import json from './assets/data.json';
+import webpackLogo from './assets/webpack_logo.png';
 import './styles/styles.css';
 
-const post = new Post('Webpack Post Title');
+const post = new Post('Webpack Post Title', webpackLogo);
 
 console.log(`Post to string: ${post}`);
+
+console.log('JSON:', json);
